46000027b927644affc985fab135b601c0b033ba
Ouissal Bouamar <ouissalbouamar@gmail.com>
2024-05-31 16:52:41 -0700

    seconde commit

--- APP/urls.py
+++ APP/urls.py
@@ -6,5 +6,11 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('book/<int:id>', views.book, name='book'),
     path('create_book/', views.create_book, name='create_book'),
-
+    path('update_book/<int:id>', views.update_book, name='update_book'),
+    path('delete_book/<int:id>', views.delete_book, name='delete_book'),
+    path('home/', views.home_author, name='home'),
+    path('create_author/', views.create_author, name='create_author'),
+    path('update_author/<int:id>/', views.update_author, name='update_author'),
+    path('delete_author/<int:id>/', views.delete_author, name='delete_author'),
 ]
+   
--- APP/views.py
+++ APP/views.py
@@ -5,22 +5,9 @@ def home(request):
     books = Book.objects.all()
 
     books_list = [
-        {
-        'title' : 'Book1',
-        'price': 100,
-        'publication_year':1999,
-        },
-        {
-        'title' : 'Book2',
-        'price': 100,
-        'publication_year':1999,
-        },
-        {
-        'title' : 'Book3',
-        'price': 100,
-        'publication_year':1999,
-        }                                  
-        
+        {'title': 'Book1', 'price': 100, 'publication_year': 1999},
+        {'title': 'Book2', 'price': 100, 'publication_year': 1999},
+        {'title': 'Book3', 'price': 100, 'publication_year': 1999}               
     ]
 
     home_page_context = {
@@ -42,11 +29,75 @@ def book(request, id):
 
 
 def create_book(request):
-    author = Author.objects.get(id=1)
+    authors = Author.objects.all()
     if request.method == 'POST':
         title = request.POST.get('title')
         genre = request.POST.get('genre')
         price = request.POST.get('price')
-        Book.objects.create(title=title, genre=genre, price=price, author=author)
+        publication_year = request.POST.get('publication_year')
+        author_id = request.POST.get('author')
+        author = Author.objects.get(id=author_id)
+        Book.objects.create(title=title, genre=genre, price=price, publication_year=publication_year, author=author)
         return redirect('home_page')
-    return render(request, 'create_book.html')
+    context = { 
+        'authors': authors
+    }
+    return render(request, 'create_book.html', context=context)
+
+def update_book(request, id):
+    book = Book.objects.get(id=id)
+    authors = Author.objects.all()
+    if request.method == 'POST':
+        book.title = request.POST.get('title')
+        book.genre = request.POST.get('genre')
+        book.price = request.POST.get('price')
+        book.publication_year = request.POST.get('publication_year')
+        author_id = request.POST.get('author')
+        book.author = Author.objects.get(id=author_id)
+        book.save()
+        return redirect('home_page')
+    context = { 
+        'book': book,
+        'authors': authors,   
+    }
+    return render(request, 'update_book.html', context=context)
+
+def delete_book(request, id):
+    book = Book.objects.get(id=id)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('home_page')
+    context = {   
+        'book': book,   
+    }
+    return render(request, 'delete_book.html', context=context)
+
+def home_author(request):
+    authors = Author.objects.all()
+    return render(request, 'home_author.html', {'authors': authors})
+
+def create_author(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        birth_date = request.POST.get('birth_date')
+        nationality = request.POST.get('nationality')
+        Author.objects.create(name=name, birth_date=birth_date, nationality=nationality)
+        return redirect('home')
+    return render(request, 'create_author.html')
+
+def update_author(request, id):
+    author = Author.objects.get(id=id)
+    if request.method == 'POST':
+        author.name = request.POST.get('name')
+        author.birth_date = request.POST.get('birth_date')
+        author.nationality = request.POST.get('nationality')
+        author.save()
+        return redirect('home')
+    return render(request, 'update_author.html', {'author': author})
+
+def delete_author(request, id):
+    author = Author.objects.get(id=id)
+    if request.method == 'POST':
+        author.delete()
+        return redirect('home')
+    return render(request, 'delete_author.html', {'author': author})
